5d29597a62d4d0e2c6f9ef3a705f0b2be4797dd6
quartzshard <matthew.burgess50@gmail.com>
2019-10-16 19:27:18 +0100

    added installer and changed paths appropriately

---
+++ luks-mount-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Quartzshard, 2019
+
+#Check is script is running as root
+if [[ $EUID > 0 ]]; then
+	echo 'Please run as root (sudo ./luks-mount-install...)'
+	exit
+fi
+
+mv ./luks-mount.sh /usr/local/bin/luks-mount
+mv ./luks-mount.shlib /usr/local/lib/
+mv ./luks-mount.cfg /etc/
+chmod a+x /usr/local/bin/luks-mount
